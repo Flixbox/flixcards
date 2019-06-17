a994a2f8b16cd69d599cf5c6dec7287f9fed0ae9
Felix <Felix.Tietjen@txs.de>
2019-06-17 13:58:40 +0200

    feat: fixed stateReconciler

--- components/ReduxProvider.js
+++ components/ReduxProvider.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 
 import { persistStore, persistReducer } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import ExpoFileSystemStorage from 'redux-persist-expo-filesystem'
 
 import thunkMiddleware from 'redux-thunk'
@@ -15,7 +15,7 @@ import reducer from '../reducers'
 const persistConfig = {
     key: 'root',
     storage: ExpoFileSystemStorage,
-    stateReconciler: autoMergeLevel2,
+    // stateReconciler: autoMergeLevel2,
 }
 
 const store = createStore(persistReducer(persistConfig, reducer), applyMiddleware(thunkMiddleware))
