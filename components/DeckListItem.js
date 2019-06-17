afeae3a6a00bd2412d261f9efdaffd35ddde5cf9
Felix <Felix.Tietjen@txs.de>
2019-06-17 16:07:26 +0200

    feat: enhanced deck list item styling

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Card, CardItem, Text, Button } from 'native-base'
+import { Card, CardItem, Text, Button, Left, Right, Icon } from 'native-base'
 
 /**
  * @returns {React.Element} A single card with general deck properties.
  */
 const DeckListItem = ({ id, name }) => (
-    <Card>
-        <CardItem header>
-            <Button transparent>
-                <Text>{name}</Text>
-            </Button>
-        </CardItem>
-    </Card>
+    <Button transparent block>
+        <Left>
+            <Text>{name}</Text>
+        </Left>
+        <Right>
+            <Icon type="AntDesign" name="rightcircleo" />
+        </Right>
+    </Button>
 )
 
 export default DeckListItem
