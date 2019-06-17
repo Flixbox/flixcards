81cdeddb9f40d758c7061fe41948d7abaf30f672
Felix <Felix.Tietjen@txs.de>
2019-06-17 14:08:40 +0200

    feat: hooked up the main deck view to redux

--- screens/DeckListScreen.js
+++ screens/DeckListScreen.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { Container, Content, Text } from 'native-base'
+import { Content, Text } from 'native-base'
+import { connect } from 'react-redux'
 
 import Components from '../components'
 
-export default (DeckListScreen = () => (
+const DeckListScreen = ({ decks }) => (
     <Content>
-        <Text>Here's a bunch of cards!</Text>
-        <Components.DeckListItem />
-        <Components.DeckListItem />
-        <Components.DeckListItem />
-        <Components.DeckListItem />
-        <Components.DeckListItem />
-        <Components.DeckListItem />
+        <Text>{"Here's a bunch of cards!"}</Text>
+        {decks.map(deck => (
+            <Components.DeckListItem {...deck} key={deck.id} />
+        ))}
     </Content>
-))
+)
 
 DeckListScreen.navigationOptions = {
     header: null,
 }
+
+const mapStateToProps = ({ decks }) => {
+    return {
+        decks,
+    }
+}
+
+export default connect(mapStateToProps)(DeckListScreen)
