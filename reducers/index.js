3f8eb00366525a970ac4f88eaa636aa2387fa08a
Felix <Felix.Tietjen@txs.de>
2019-06-18 10:04:49 +0200

    feat: added redux handling for new decks

--- reducers/index.js
+++ reducers/index.js
@@ -1,3 +1,23 @@
 import defaultState from '../helpers/defaultState'
+import { CREATE_DECK } from '../actions/decks'
+import uuid from 'uuid/v4'
 
-export default (state = defaultState, action) => state
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        case CREATE_DECK:
+            return {
+                ...state,
+                decks: [
+                    ...state.decks,
+                    {
+                        id: uuid(),
+                        name: action.name,
+                        cards: [],
+                    },
+                ],
+            }
+        default:
+            console.log('Current state: ', state)
+            return state
+    }
+}
