49995a455022563b0345c2c70c8ba4f0f49952ad
Felix <Felix.Tietjen@txs.de>
2019-06-18 09:47:56 +0200

    feat: enhanced NewDeck screen

--- screens/NewDeck.js
+++ screens/NewDeck.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
-import { Content, Button, Text } from 'native-base'
+import { Content, Button, Text, Form, Input, Item } from 'native-base'
 import { connect } from 'react-redux'
 
-import Components from '../components'
 import { createDeck } from '../actions/decks'
 
 const NewDeck = ({ dispatch }) => {
     const [state, setState] = useState({})
 
+    function changeText(name) {
+        setState({ name })
+    }
+
     function newDeck() {
         dispatch(createDeck(state.name))
     }
+
     return (
         <Content>
-            <Text>New Deck</Text>
+            <Form>
+                <Item>
+                    <Input underline placeholder="Title" value={state.name} onChangeText={changeText} />
+                </Item>
+            </Form>
             <Button block onPress={newDeck}>
                 <Text>Create deck</Text>
             </Button>
