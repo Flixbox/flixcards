75c3638758c25a6b2c43037ac58eeb6984746fde
Felix <Felix.Tietjen@txs.de>
2019-06-18 14:56:38 +0200

    feat: added new card screen

---
+++ screens/NewCard.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { Content, Button, Text, Form, Input, Item } from 'native-base'
+import { connect } from 'react-redux'
+
+import { createCard } from '../actions/decks'
+
+const NewCard = ({ deckId, dispatch, navigation }) => {
+    const [state, setState] = useState({})
+
+    function changeQuestion(question) {
+        setState({ question })
+    }
+
+    function changeAnswer(answer) {
+        setState({ answer })
+    }
+
+    function newCard() {
+        const { question, answer } = state
+        dispatch(createCard(deckId, question, answer))
+        setState({})
+        navigation.goBack()
+    }
+
+    return (
+        <Content>
+            <Form>
+                <Item>
+                    <Input underline placeholder="Question" value={state.question} onChangeText={changeQuestion} />
+                </Item>
+                <Item>
+                    <Input
+                        underline
+                        placeholder="Answer"
+                        value={state.answer}
+                        onChangeText={changeAnswer}
+                        onSubmitEditing={newCard}
+                    />
+                </Item>
+                <Button block onPress={newCard}>
+                    <Text>Create card</Text>
+                </Button>
+            </Form>
+        </Content>
+    )
+}
+
+NewCard.navigationOptions = {
+    title: 'New card',
+}
+
+const mapDispatchToProps = dispatch => dispatch
+
+export default connect(mapDispatchToProps)(NewCard)
--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,11 @@
 import DeckListScreen from './DeckListScreen'
 import NewDeck from './NewDeck'
+import NewCard from './NewCard'
 import DeckDetails from './DeckDetails'
 
 export default {
     DeckListScreen,
     NewDeck,
+    NewCard,
     DeckDetails,
 }
