4f40a1a8725e283b006296d50ae4fa184052d2da
Felix <Felix.Tietjen@txs.de>
2019-06-19 10:29:15 +0200

    feat: enhanced deck swiper and provided correct data

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
-import { Content, Button, Text, Form, Input, Item, Card, CardItem, Body, Left, Right, Icon } from 'native-base'
+import { Content, Button, Text, Card, CardItem, Body, Left, Right, Icon, DeckSwiper, View } from 'native-base'
 import { connect } from 'react-redux'
 import { StackActions, NavigationActions } from 'react-navigation'
 import uuid from 'uuid/v4'
 
-const Quiz = ({ dispatch, navigation, deckId, deck }) => {
+const Quiz = ({ navigation }) => {
+    const { deckId, deck } = navigation.state.params
     const [state, setState] = useState({
         finished: true,
-        score: '10/10',
+        score: {
+            correct: 0,
+            incorrect: 0,
+        },
     })
+
+    const { cards } = deck
+    const cardsArray = Object.keys(cards).map(id => ({ id, ...cards[id] }))
+    let deckSwiper
 
     function goBack() {
         navigation.goBack()
@@ -18,7 +26,29 @@ const Quiz = ({ dispatch, navigation, deckId, deck }) => {
         navigation.goBack()
     }
 
-    return <Content>{state.finished && <Finished score={state.score} goBack={goBack} restart={restart} />}</Content>
+    function renderEmpty() {
+        return <Finished score={state.score} goBack={goBack} restart={restart} />
+    }
+
+    function renderItem(card) {
+        return <QuizCard {...card} />
+    }
+
+    function assignDeckSwiperRef(c) {
+        deckSwiper = c
+    }
+
+    return (
+        <Content contentContainerStyle={{ flex: 1 }}>
+            <DeckSwiper
+                ref={assignDeckSwiperRef}
+                dataSource={cardsArray}
+                renderItem={renderItem}
+                renderEmpty={renderEmpty}
+                looping={false}
+            />
+        </Content>
+    )
 }
 
 const Finished = ({ goBack, restart, score }) => (
@@ -42,6 +72,33 @@ const Finished = ({ goBack, restart, score }) => (
                 <Button transparent onPress={restart}>
                     <Text>Restart quiz</Text>
                     <Icon type="AntDesign" name="retweet" />
+                </Button>
+            </Right>
+        </CardItem>
+    </Card>
+)
+
+const QuizCard = ({ question, answer, guessCorrect, guessIncorrect }) => (
+    <Card>
+        <CardItem header>
+            <Text>Question</Text>
+        </CardItem>
+        <CardItem>
+            <Body>
+                <Text>{question}</Text>
+            </Body>
+        </CardItem>
+        <CardItem footer>
+            <Left>
+                <Button transparent onPress={guessIncorrect}>
+                    <Icon type="AntDesign" name="minuscircleo" />
+                    <Text>Incorrect</Text>
+                </Button>
+            </Left>
+            <Right>
+                <Button transparent onPress={guessCorrect}>
+                    <Text>Correct</Text>
+                    <Icon type="AntDesign" name="pluscircleo" />
                 </Button>
             </Right>
         </CardItem>
